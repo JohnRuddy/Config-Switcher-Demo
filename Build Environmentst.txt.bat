762ece8b969f5d565e24cc818138f588b3555688
John Ruddy <john.ruddy@charteredaccountants.ie>
2019-07-23 08:32:45 +0100

    Initial Commit

---
+++ Build Environmentst.txt.bat
@@ -0,0 +1,4 @@
+MsBuild.exe C:\Projects\Config-Switcher-Demo\Config-Switcher-Demo.sln /t:Clean,Build /p:Configuration=UAT /p:TargetFramework=v4.0
+MsBuild.exe C:\Projects\Config-Switcher-Demo\Config-Switcher-Demo.sln /t:Clean,Build /p:Configuration=Live /p:TargetFramework=v4.0
+MsBuild.exe C:\Projects\Config-Switcher-Demo\Config-Switcher-Demo.sln /t:Clean,Build /p:Configuration=Release /p:TargetFramework=v4.0
+MsBuild.exe C:\Projects\Config-Switcher-Demo\Config-Switcher-Demo.sln /t:Clean,Build /p:Configuration=Debug /p:TargetFramework=v4.0
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Config_Switcher_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var msg = string.Format("Welcome to the {0} Environment", ConfigurationManager.AppSettings["ApplicationEnvironment"]);
+            Console.WriteLine(msg);
+            Console.WriteLine("Press Any Key..");
+            Console.ReadKey();
+
+        }
+    }
+}
